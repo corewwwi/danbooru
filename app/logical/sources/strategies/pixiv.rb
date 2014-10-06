4eb68f80e74e1ccb924e3fb74b303e4bcf69a21a
r888888888 <r888888888@gmail.com>
2014-10-06 14:20:17 -0700

    integrate ugoira converted into upload flow

--- app/logical/sources/strategies/pixiv.rb
+++ app/logical/sources/strategies/pixiv.rb
@@ -33,6 +33,31 @@ module Sources
         end
       end
 
+      def agent
+        @agent ||= begin
+          mech = Mechanize.new
+
+          phpsessid = Cache.get("pixiv-phpsessid")
+          if phpsessid
+            cookie = Mechanize::Cookie.new("PHPSESSID", phpsessid)
+            cookie.domain = ".pixiv.net"
+            cookie.path = "/"
+            mech.cookie_jar.add(cookie)
+          else
+            mech.get("http://www.pixiv.net") do |page|
+              page.form_with(:action => "/login.php") do |form|
+                form['pixiv_id'] = Danbooru.config.pixiv_login
+                form['pass'] = Danbooru.config.pixiv_password
+              end.click_button
+            end
+            phpsessid = mech.cookie_jar.cookies.select{|c| c.name == "PHPSESSID"}.first
+            Cache.put("pixiv-phpsessid", phpsessid.value, 1.month) if phpsessid
+          end
+
+          mech
+        end
+      end
+      
     protected
 
       def get_profile_from_page(page)
@@ -112,30 +137,6 @@ module Sources
         end
       end
 
-      def agent
-        @agent ||= begin
-          mech = Mechanize.new
-
-          phpsessid = Cache.get("pixiv-phpsessid")
-          if phpsessid
-            cookie = Mechanize::Cookie.new("PHPSESSID", phpsessid)
-            cookie.domain = ".pixiv.net"
-            cookie.path = "/"
-            mech.cookie_jar.add(cookie)
-          else
-            mech.get("http://www.pixiv.net") do |page|
-              page.form_with(:action => "/login.php") do |form|
-                form['pixiv_id'] = Danbooru.config.pixiv_login
-                form['pass'] = Danbooru.config.pixiv_password
-              end.click_button
-            end
-            phpsessid = mech.cookie_jar.cookies.select{|c| c.name == "PHPSESSID"}.first
-            Cache.put("pixiv-phpsessid", phpsessid.value, 1.month) if phpsessid
-          end
-
-          mech
-        end
-      end
     end
   end
 end
