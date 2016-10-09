17e6d8d12812c45c78ccf9ef9082dddf650698d7
evazion <noizave@gmail.com>
2016-10-09 08:08:19 +0000

    Cleanup to_escaped_for_sql_like, to_escaped_for_tsquery_split.

--- config/initializers/core_extensions.rb
+++ config/initializers/core_extensions.rb
@@ -2,11 +2,11 @@ module Danbooru
   module Extensions
     module String
       def to_escaped_for_sql_like
-        return self.gsub(/\\/, '\0\0').gsub(/(%|_)/, "\\\\\\1").gsub(/\*/, '%')
+        gsub(/\\/, '\0\0').gsub(/(%|_)/, "\\\\\\1").gsub(/\*/, '%')
       end
 
       def to_escaped_for_tsquery_split
-        scan(/\S+/).map {|x| x.to_escaped_for_tsquery}.join(" & ")
+        scan(/\S+/).map(&:to_escaped_for_tsquery).join(" & ")
       end
 
       def to_escaped_for_tsquery
