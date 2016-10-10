2c64356e1414fd4299081e13c24490eb481a2308
evazion <noizave@gmail.com>
2016-10-10 05:31:49 +0000

    PostReadOnly: silence '`*' interpreted as argument prefix' warning.

--- app/models/post_read_only.rb
+++ app/models/post_read_only.rb
@@ -1,6 +1,6 @@
 class PostReadOnly < Post
   establish_connection "ro_#{Rails.env}".to_sym
-  attr_readonly *column_names
+  attr_readonly(*column_names)
 
   def with_timeout(n, default_value = nil)
     connection.execute("SET STATEMENT_TIMEOUT = #{n}") unless Rails.env == "test"
