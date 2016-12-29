ce932c539cc9b0caf832e1ab886bf8fb1bed15c4
evazion <noizave@gmail.com>
2016-12-29 20:05:01 +0000

    /posts/123: Don't crash on invalid parent ID.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
     @post_flag = PostFlag.new(:post_id => @post.id)
     @post_appeal = PostAppeal.new(:post_id => @post.id)
     
-    include_deleted = @post.is_deleted? || (@post.parent_id.present? && @post.parent.is_deleted?) || CurrentUser.user.show_deleted_children?
+    include_deleted = @post.is_deleted? || (@post.parent.present? && @post.parent.is_deleted?) || CurrentUser.user.show_deleted_children?
     @parent_post_set = PostSets::PostRelationship.new(@post.parent_id, :include_deleted => include_deleted)
     @children_post_set = PostSets::PostRelationship.new(@post.id, :include_deleted => include_deleted)
     respond_with(@post)
