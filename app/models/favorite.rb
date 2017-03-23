259516022d8d039ff6eaacd70669e0338658d442
evazion <noizave@gmail.com>
2017-03-23 00:10:39 -0500

    favorites.rb: add move favorite method.

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -14,7 +14,29 @@ class Favorite < ActiveRecord::Base
     Favorite.destroy_all(user_id: user.id, post_id: post.id)
   end
 
+  def move(other_post)
+    other_fav = other_post.favorites.find { |f| f.user_id == user_id }
+
+    # if the user hasn't already fav'd the other post, move this fav to the other post.
+    if other_fav.nil?
+      self.update(post: other_post)
+    else # if the user has already fav'd the other post...
+      if other_fav.id < id
+        # ...and the other fav is older than this fav, then keep the other fav and remove this fav.
+        self.destroy
+      else
+        # ...and the other fav is newer than this fav, then replace the other fav with this fav.
+        other_fav.destroy
+        self.update(post: other_post)
+      end
+    end
+  end
+
   def update_post
+    if post_id_changed? && post_id_was.present?
+      Post.find(post_id_was).regen_fav_string!
+    end
+
     post.regen_fav_string!
   end
 end
