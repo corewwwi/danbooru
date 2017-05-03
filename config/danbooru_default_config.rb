52fa8cd1decd192ee4dfc1a9cf93f0b06bf4c181
evazion <noizave@gmail.com>
2017-05-03 11:05:04 -0500

    danbooru_default_config.rb: add pawoo credentials.

--- config/danbooru_default_config.rb
+++ config/danbooru_default_config.rb
@@ -488,6 +488,13 @@ module Danbooru
     def twitter_api_secret
     end
 
+    # http://tinysubversions.com/notes/mastodon-bot/
+    def pawoo_client_id
+    end
+
+    def pawoo_client_secret
+    end
+
     def enable_post_search_counts
       false
     end
